ec0f8a1a53c5e82ceaff61b85e2bb80c67925c0f
Glauciorsantos <68239593+Glauciorsantos@users.noreply.github.com>
2020-07-17 21:20:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 warnings.filterwarnings("ignore")
 
 # import model
-with open('model/covid_model.pkl', 'rb') as file:
+with open('covid_model.pkl', 'rb') as file:
 
     covid_model = pickle.load(file)
 
@@ -72,4 +72,4 @@ def main():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
